3c386b596bf12b105e781b2cc8b9a95acdfdcdfd
emperor31 <sairamsingireesu@gmail.com>
2017-03-20 22:04:28 +0530

    game form fix,google image api

--- games_tutorial/app/controllers/application_controller.rb
+++ games_tutorial/app/controllers/application_controller.rb
@@ -2,12 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user
-private def current_user
-  if session[:name]
-  		@current_user = session[:name]
-	else 
-		nil
-   end
-end
+  helper_method :current_user, :google_search
+	private def current_user
+	  if session[:name]
+	  		@current_user = session[:name]
+		else 
+			nil
+	   end
+	end
+	private def google_search
+		GoogleCustomSearchApi.search("poker")	
+	end
 end
